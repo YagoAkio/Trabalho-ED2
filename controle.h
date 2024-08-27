ee0517d6bab73d13b45c6d8d1256104216b7f646
YagoAkio <143807170+YagoAkio@users.noreply.github.com>
2024-08-26 23:31:31 -0300

    ideia

--- controle.h
+++ controle.h
@@ -1,5 +1,7 @@
 #include "modelo.h"
 
 void executar(){
-    exibir_programa_TAD(ler_programa(abrir_programa("arquivo.py")));
+    exibir_arquivo_fscanf(abrir_programa("arquivo.py"));
+    exibir_arquivo_get(abrir_programa("arquivo.py"));
+    //exibir_programa_TAD(ler_programa(abrir_programa("arquivo.py")));
 }
--- modelo.h
+++ modelo.h
@@ -1,4 +1,4 @@
-#define tamanho_string 20
+#define tamanho_string 50
 
 struct token{
     char info[tamanho_string];
@@ -28,10 +28,17 @@ FILE *abrir_programa(char nome_arquivo[20]){
     return fopen(nome_arquivo, "rb");
 }
 
-void exibir_arquivo(FILE *arquivo){
+void exibir_arquivo_get(FILE *arquivo){
     char caracter;
     while((caracter = fgetc(arquivo))!=EOF){
         printf("%c",caracter);
+    }
+}
+
+void exibir_arquivo_fscanf(FILE *arquivo){
+    char informacao_linha_arquivo[tamanho_string];
+    while (fscanf(arquivo, "%[^\n]\n",&informacao_linha_arquivo)!=EOF){
+        printf("%s\n",informacao_linha_arquivo);
     }
 }
 
@@ -56,31 +63,56 @@ linha = linha->prox;
 linha->ant = linha_auxiliar;
 */
 
-Linha *ler_programa (FILE *arquivo){
-    char caracter;
-    int contador_auxiliar=0;
+//Linha *ler_programa (FILE *arquivo){
+//    char caracter;
+//    int contador_auxiliar=0;
+//    Token *token;
+//    Linha *linha, *linha_auxiliar;
+//    linha = criar_linha_vazia();
+//    token = linha->token;
+//    while((caracter = fgetc(arquivo))!=EOF){
+//        if(caracter == ' ' || caracter == '\n'){
+//            token->info[++contador_auxiliar] = '\0';
+//            contador_auxiliar = 0;
+//            token -> prox = criar_token_vazio();
+//            token = token -> prox;
+//            if(caracter == '\n'){
+//                linha->prox = criar_linha_vazia();
+//                linha_auxiliar = linha;
+//                linha = linha->prox;
+//                linha->ant = linha_auxiliar;
+//                token = linha->token;
+//            }
+//        }else{
+//            token->info[contador_auxiliar++] = caracter;
+//        }
+//    }
+//    return linha;
+//}
+
+Token *quebrar_linha(char informacao[tamanho_string]){
     Token *token;
-    Linha *linha, *linha_auxiliar;
+    token = criar_token_vazio();
+    strcpy(token->info,informacao);
+    return token;
+}
+
+Linha *ler_programa(FILE *arquivo){
+    Linha *linha_retorno, *linha, *linha_auxiliar;
+    char informacao_linha_arquivo[tamanho_string];
+
     linha = criar_linha_vazia();
-    token = linha->token;
-    while((caracter = fgetc(arquivo))!=EOF){
-        if(caracter == ' ' || caracter == '\n'){
-            token->info[++contador_auxiliar] = '\0';
-            contador_auxiliar = 0;
-            token -> prox = criar_token_vazio();
-            token = token -> prox;
-            if(caracter == '\n'){
-                linha->prox = criar_linha_vazia();
-                linha_auxiliar = linha;
-                linha = linha->prox;
-                linha->ant = linha_auxiliar;
-                token = linha->token;
-            }
-        }else{
-            token->info[contador_auxiliar++] = caracter;
-        }
+    linha_retorno = linha;
+
+    while(fscanf(arquivo, "%[^\n]\n",&informacao_linha_arquivo)!=EOF){
+        linha->token->prox = quebrar_linha(informacao_linha_arquivo);
+        linha_auxiliar = criar_linha_vazia();
+        linha->prox = linha_auxiliar;
+        linha_auxiliar->ant = linha;
+        linha = linha->prox;
     }
-    return linha;
+
+    return linha_retorno;
 }
 
 void exibir_programa_TAD (Linha *programa){
